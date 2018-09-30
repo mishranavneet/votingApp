c94c3bde4307a17004037c6b60cea8dafc5bf865
Navneet Mishra <navneetmishra25@gmail.com>
2018-10-01 00:48:01 +0530

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';  
+import { NgModule } from '@angular/core';   
+  
+import { HttpModule } from '@angular/http';  
+import { FormsModule } from '@angular/forms';  
+  
+import { AppComponent } from './app.component';  
+  
+import {CommonService} from './common.service';  
+import { MemberListComponent } from './member/member-list.component';
+import { MemberListService } from './member/member-list.service';
+
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+  
+@NgModule({  
+  declarations: [  
+    AppComponent,
+    MemberListComponent  
+  ],  
+  imports: [  
+    BrowserModule,HttpModule,FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      // {path:'Report' , component:ReportComponent}, 
+      {path:'Voting' , component:MemberListComponent}
+      // {path: 'sprint-dashboard' , component:MemberListComponent}
+      
+    ])
+  ],  
+  providers: [CommonService,MemberListService],  
+  bootstrap: [AppComponent]  
+})  
+export class AppModule { }  
---
+++ src/app/common.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';   
+import {Http,Response, Headers, RequestOptions } from '@angular/http';   
+import {map} from 'rxjs/operators';
+  
+@Injectable()  
+export class CommonService {  
+  
+  constructor(private http: Http) { }  
+  
+  saveUser(model){      
+    return this.http.post('http://localhost:8080/api/SaveUser/', model).pipe(map((response: Response) =>response.json()))
+            
+  }  
+  
+  GetUser(){       
+    return this.http.get('http://localhost:8080/api/getUser/').pipe(map((response: Response) => response.json()))
+            
+  }  
+ deleteUser(id){   
+    return this.http.post('http://localhost:8080/api/deleteUser/',{'id': id}).pipe(map((response: Response) =>response.json()))
+                  
+  }
+  
+  // GetMember(){       
+  //   return this.http.get('http://localhost:8080/api/getUser/').pipe(map((response: Response) => response.json()))
+            
+  // }
+  
+} 
